f8cb8ce8433949fe6db04ff8ad4f6b0b96c1ae76
puckey <jonathan@puckey.studio>
2025-10-02 16:11:44 +0200

    refactor(ios): remove DispatchQueueType protocol and unused test initializers

--- ios/NowPlayingInfo/NowPlayingInfoController.swift
+++ ios/NowPlayingInfo/NowPlayingInfoController.swift
@@ -9,7 +9,7 @@ import Foundation
 import MediaPlayer
 
 public class NowPlayingInfoController: NowPlayingInfoControllerProtocol {
-  private var infoQueue: DispatchQueueType = DispatchQueue(
+  private var infoQueue = DispatchQueue(
     label: "NowPlayingInfoController.infoQueue",
     attributes: .concurrent
   )
@@ -19,16 +19,6 @@ public class NowPlayingInfoController: NowPlayingInfoControllerProtocol {
 
   public required init() {
     infoCenter = MPNowPlayingInfoCenter.default()
-  }
-
-  /// Used for testing purposes.
-  public required init(dispatchQueue: DispatchQueueType, infoCenter: NowPlayingInfoCenter) {
-    infoQueue = dispatchQueue
-    self.infoCenter = infoCenter
-  }
-
-  public required init(infoCenter: NowPlayingInfoCenter = MPNowPlayingInfoCenter.default()) {
-    self.infoCenter = infoCenter
   }
 
   public func set(keyValues: [NowPlayingInfoKeyValue]) {
