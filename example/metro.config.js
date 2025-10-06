9d0e02062d6703b31a314af57454bd72315491a9
puckey <jonathan@puckey.studio>
2025-10-06 16:34:33 +0200

    refactor(example): remove Reanimated dependencies from build config
    Remove react-native-reanimated/plugin from Babel config and wrapWithReanimatedMetroConfig from Metro config

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,9 +1,6 @@
 const path = require('path');
 const { getDefaultConfig } = require('@react-native/metro-config');
 const { withMetroConfig } = require('react-native-monorepo-config');
-const {
-  wrapWithReanimatedMetroConfig,
-} = require('react-native-reanimated/metro-config');
 
 const root = path.resolve(__dirname, '..');
 
@@ -13,10 +10,7 @@ const root = path.resolve(__dirname, '..');
  *
  * @type {import('metro-config').MetroConfig}
  */
-module.exports = withMetroConfig(
-  wrapWithReanimatedMetroConfig(getDefaultConfig(__dirname)),
-  {
-    root,
-    dirname: __dirname,
-  }
-);
+module.exports = withMetroConfig(getDefaultConfig(__dirname), {
+  root,
+  dirname: __dirname,
+});
