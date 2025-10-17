3e8e6edfe92cb3525a8b422cfc8a18690d079b0d
puckey <jonathan@radio.garden>
2025-10-17 15:50:47 +0200

    fix(android): correct playable flag logic in Track model
    The playable flag was incorrectly set to true when both url and uri were null.
    Fixed to set playable to true when either url or uri is present.

--- android/src/main/java/com/doublesymmetry/trackplayer/model/Track.kt
+++ android/src/main/java/com/doublesymmetry/trackplayer/model/Track.kt
@@ -54,7 +54,7 @@ private constructor(
       }
     // TODO: A playable item can still be browsable and a browsable item can be playable,
     // this logic needs to be revisited:
-    var playable = url == null && uri == null
+    var playable = url != null || uri != null
     val mediaMetadata =
       MediaMetadata.Builder()
         .setTitle(title)
