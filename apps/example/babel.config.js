9cb06ee1bf157c0b92af57c90d2f469b35c6d788
Jacob Spizziri <jspizziri@weare5stones.com>
2025-10-21 13:40:49 -0400

    ref(example): refactor the example app and add a nextjs one

--- apps/example/babel.config.js
+++ apps/example/babel.config.js
@@ -7,6 +7,17 @@ const root = path.resolve(__dirname, '..', '..');
 module.exports = getConfig(
   {
     presets: ['module:@react-native/babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@': '../common-app/src',
+          },
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.jpg', '.m4a'],
+        },
+      ],
+    ],
   },
   { root, pkg }
 );
---
+++ apps/nextjs/pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import TrackPlayerApp from '../components/TrackPlayerApp';
+
+export default function App() {
+  return (
+    <TrackPlayerApp />
+  );
+}
