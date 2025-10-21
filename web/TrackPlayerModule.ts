047ba85fda68c04ac9446915833c906ca3bde2d5
Jacob Spizziri <jspizziri@weare5stones.com>
2025-10-21 13:40:10 -0400

    feat(web): update the web implementation to conform to the new API

--- web/TrackPlayerModule.ts
+++ web/TrackPlayerModule.ts
@@ -11,70 +11,26 @@ const Event = {
   PlaybackActiveTrackChanged: 'playback-active-track-changed',
 };
 import type { Spec } from '../src/NativeTrackPlayer';
-import type { PlaybackState, Track, UpdateOptions } from '../src/types';
-import { PlaylistPlayer, RepeatMode } from './TrackPlayer';
+import type { PlaybackState, PlayingState, Track, UpdateOptions } from '../src/features';
+import { PlaylistPlayer } from './TrackPlayer';
 import { SetupNotCalledError } from './TrackPlayer/SetupNotCalledError';
 
 export class TrackPlayerModule extends PlaylistPlayer implements Spec {
   protected emitter = DeviceEventEmitter;
   protected progressUpdateEventInterval: NodeJS.Timeout | undefined;
 
-  public getConstants() {
-    return {
-      // Capabilities
-      CAPABILITY_PLAY: 'play',
-      CAPABILITY_PLAY_FROM_ID: 'play-from-id',
-      CAPABILITY_PLAY_FROM_SEARCH: 'play-from-search',
-      CAPABILITY_PAUSE: 'pause',
-      CAPABILITY_STOP: 'stop',
-      CAPABILITY_SEEK_TO: 'seek-to',
-      CAPABILITY_SKIP: 'skip',
-      CAPABILITY_SKIP_TO_NEXT: 'skip-to-next',
-      CAPABILITY_SKIP_TO_PREVIOUS: 'skip-to-previous',
-      CAPABILITY_SET_RATING: 'set-rating',
-      CAPABILITY_JUMP_FORWARD: 'jump-forward',
-      CAPABILITY_JUMP_BACKWARD: 'jump-backward',
-
-      // Rating Types
-      RATING_HEART: 'heart',
-      RATING_THUMBS_UP_DOWN: 'thumbs-up-down',
-      RATING_3_STARS: '3-stars',
-      RATING_4_STARS: '4-stars',
-      RATING_5_STARS: '5-stars',
-      RATING_PERCENTAGE: 'percentage',
-
-      // Pitch Algorithms
-      PITCH_ALGORITHM_LINEAR: 'linear',
-      PITCH_ALGORITHM_MUSIC: 'music',
-      PITCH_ALGORITHM_VOICE: 'voice',
-
-      // States
-      STATE_BUFFERING: 'STATE_BUFFERING',
-      STATE_LOADING: 'STATE_LOADING',
-      STATE_NONE: 'STATE_NONE',
-      STATE_PAUSED: 'STATE_PAUSED',
-      STATE_PLAYING: 'STATE_PLAYING',
-      STATE_READY: 'STATE_READY',
-      STATE_STOPPED: 'STATE_STOPPED',
-
-      // Repeat Modes
-      REPEAT_OFF: RepeatMode.Off,
-      REPEAT_TRACK: RepeatMode.Track,
-      REPEAT_QUEUE: RepeatMode.Playlist,
-    };
+  // MARK: init and config
+  public updateOptions(options: UpdateOptions) {
+    this.setupProgressUpdates(options.progressUpdateEventInterval);
   }
 
   // observe and emit state changes
-  public get state(): PlaybackState {
+  protected get state(): PlaybackState {
     return super.state;
   }
-  public set state(newState: PlaybackState) {
+  protected set state(newState: PlaybackState) {
     super.state = newState;
     this.emitter.emit(Event.PlaybackState, newState);
-  }
-
-  public async updateOptions(options: UpdateOptions) {
-    this.setupProgressUpdates(options.progressUpdateEventInterval);
   }
 
   protected setupProgressUpdates(interval?: number) {
@@ -100,19 +56,19 @@ export class TrackPlayerModule extends PlaylistPlayer implements Spec {
     }
   }
 
-  protected async onPlaylistEnded() {
-    await super.onPlaylistEnded();
+  protected onPlaylistEnded() {
+    super.onPlaylistEnded();
     this.emitter.emit(Event.PlaybackQueueEnded, {
       track: this.currentIndex,
       position: this.element!.currentTime,
     });
   }
 
-  public get playWhenReady(): boolean {
+  public getPlayWhenReady(): boolean {
     return super.playWhenReady;
   }
 
-  public set playWhenReady(pwr: boolean) {
+  public setPlayWhenReady(pwr: boolean) {
     const didChange = pwr !== this._playWhenReady;
     super.playWhenReady = pwr;
 
@@ -121,84 +77,123 @@ export class TrackPlayerModule extends PlaylistPlayer implements Spec {
         playWhenReady: this._playWhenReady,
       });
     }
+
+    return super.playWhenReady;
   }
 
-  public async getPlayWhenReady(): Promise<boolean> {
-    return this.playWhenReady;
-  }
-
-  public async setPlayWhenReady(pwr: boolean): Promise<boolean> {
-    this.playWhenReady = pwr;
-    return this.playWhenReady;
-  }
-
-  public async load(track: Track) {
+  public async load(
+    track: Track,
+    onComplete?: (track: Track) => void,
+  ) {
     if (!this.element) throw new SetupNotCalledError();
     const lastTrack = this.current;
     const lastPosition = this.element.currentTime;
-    await super.load(track);
-
-    this.emitter.emit(Event.PlaybackActiveTrackChanged, {
-      lastTrack,
-      lastPosition,
-      lastIndex: this.lastIndex,
-      index: this.currentIndex,
-      track,
+    super.load(track, () => {
+      onComplete?.(track);
+      this.emitter.emit(Event.PlaybackActiveTrackChanged, {
+        lastTrack,
+        lastPosition,
+        lastIndex: this.lastIndex,
+        index: this.currentIndex,
+        track,
+      });
     });
   }
 
-  public async getQueue(): Promise<Track[]> {
+  public getQueue(): Track[] {
     return this.playlist;
   }
 
   public async setQueue(queue: Track[]) {
     await this.stop();
     this.playlist = queue;
+    if (queue.length) {
+      this.skip(0);
+    }
   }
 
-  public async getActiveTrack(): Promise<Track | undefined> {
+  public getActiveTrack(): Track | undefined {
     return this.current;
   }
 
-  public async getActiveTrackIndex(): Promise<number | undefined> {
+  public getActiveTrackIndex(): number | undefined {
     // per the existing spec, this should throw if setup hasn't been called
     if (!this.element || !this.player) throw new SetupNotCalledError();
     return this.currentIndex;
   }
 
-  public async getPlaybackState(): Promise<PlaybackState> {
+  public getPlaybackState(): PlaybackState {
     return this.state;
   }
 
-  /**
-   * overrides to match interface definition
-   *
-   * NOTE: these can be removed once we migrate to a sync API
-   */
-  public async pause() {
-    return super.pause();
-  }
   public async togglePlayback() {
     return super.togglePlayback();
   }
-  public async seekBy(seconds: number) {
-    return super.seekBy(seconds);
+
+  // MARK: playingState
+  public getPlayingState(state?: PlaybackState): PlayingState {
+    const curState = state ? state.state : this.state.state;
+    return {
+      playing: curState === State.Playing,
+      buffering: curState === State.Buffering
+    };
   }
-  public async seekTo(seconds: number) {
-    return super.seekTo(seconds);
+
+  public onPlaybackPlayingState(
+    callback: (state: PlayingState) => void
+  ) {
+    return this.emitter.addListener(
+      Event.PlaybackState,
+      (state: PlaybackState) => {
+        return callback(this.getPlayingState(state));
+      }
+    );
   }
-  public async setVolume(volume: number) {
-    return super.setVolume(volume);
+
+  // MARK: errors
+  public getPlaybackError() {
+    if (this.state.state === State.Error) {
+      return this.state.error?.error || null;
+    }
+    return null;
   }
-  // @ts-expect-error - promise return
-  public async getVolume() {
-    return super.getVolume();
+
+  public onPlaybackError(callback: (event: { error?: unknown }) => void) {
+    return this.emitter.addListener(Event.PlaybackError, callback);
   }
-  // @ts-expect-error - promise return
-  public async setRate(rate: number) {
-    return super.setRate(rate);
+
+  // MARK: progress
+  public onPlaybackProgressUpdated(
+    callback: (event: {
+      position: number;
+      duration: number;
+      buffered: number;
+      track: number;
+    }) => void
+  ) {
+    return this.emitter.addListener(Event.PlaybackProgressUpdated, callback);
+  }
+
+  // MARK: activeTrack
+  public onPlaybackActiveTrackChanged(callback: (event: object) => void) {
+    return this.emitter.addListener(Event.PlaybackActiveTrackChanged, callback);
   }
 
   public async acquireWakeLock() {}
   public async abandonWakeLock() {}
+
+  // MARK: Android Auto media browser stubs
+  // @ts-expect-error - these are stubs
+  public onGetItemRequest() { return { remove: () => {} }; }
+  public resolveGetItemRequest() {}
+
+  // @ts-expect-error - these are stubs
+  public onGetChildrenRequest() { return { remove: () => {} }; }
+  public resolveGetChildrenRequest() {}
+
+  // @ts-expect-error - these are stubs
+  public onGetSearchResultRequest() { return { remove: () => {} }; }
+  public resolveSearchResultRequest() {}
+
+  public setMediaBrowserReady() {}
 }
